0ede592c6b1aa5d586a513c3d6e583cf400dca63
yurioneix <yurioneix18@gmail.com>
2023-05-10 15:19:41 -0300

    Testando avaliador do github

--- tests/unit/services/updateProductService.test.js
+++ tests/unit/services/updateProductService.test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 const { updateProductService } = require("../../../src/services");
-const { noProduct, shortNameProduct } = require('../mocks/newProductMock')
+const { shortNameProduct } = require('../mocks/newProductMock')
 
 describe('Testes da camada Service para atualizar um produto', function () {
   afterEach(function () {
